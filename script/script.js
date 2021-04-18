1f4cccccea8d89ed3452e77884882950a1819a20
Vladimir Lovygin <vldmrlovygin@gmail.com>
2021-04-18 22:20:42 +0400

    ver. line #

--- script/script.js
+++ script/script.js
@@ -1,4 +1,2 @@
-var num = Number (prompt("Введите число",""));
-         if (num < 10) alert ("Твое число меньше 10");
-        else if (num < 100) alert ("Твое число болешь 10, но меньше 100");
-        else alert ("Твое число больше 100");
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
